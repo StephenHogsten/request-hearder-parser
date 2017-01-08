845083e5e0389c477ccb9f218892c152bd380ad1
Hog Dog <throweymcthrowerson@gmail.com>
2017-01-08 22:26:42 +0000

    I think I'm done?

--- server.js
+++ server.js
@@ -5,5 +5,17 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/api/whoami', function(req, res) {
+   var retJSON = {
+       ipaddress: req.headers['x-forwarded-for'],
+       language: req.headers['accept-language'].split(',')[0],
+       software: req.headers['user-agent'].split(/[\(\)]/)[1]
+   };
+   res.json(retJSON);
+   console.log(req.headers);
+   res.end();
+});
 
-app.listen(8080);
+app.listen(8080, function(path, info) {
+    console.log('new connection: ' + info);
+});
